e88fce0c654a3e425b87293cc570665c5f510fcb
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-19 23:33:45 +0200

    Create 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,9 @@
 # installs flask using pip3
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  name     => 'werkzeug',
+  provider => 'pip3',
+}
 package { 'flask':
 ensure   => '2.1.0',
 name     => 'flask',
