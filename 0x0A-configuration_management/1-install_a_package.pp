5f4a11b864db1231ca208128e5e9ea0d0984d2c8
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-19 18:42:33 +0200

    Create 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# creates a a file in /tmp
-package {'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3',
-  command  => '/usr/bin/pip3',
+# installs flask using pip3
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip3'
 }
