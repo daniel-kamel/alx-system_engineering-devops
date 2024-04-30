eaa9b123b65992d5f574000b66dccbf1c5fb89a9
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-30 17:36:30 +0300

    Create 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,9 @@
 # Configures Nginx to add a custom header to http response
 
-package { 'nginx':
-  name     => 'nginx',
-  provider => 'apt',
-  ensure   => 'installed',
-}
-
-
-exec { 'insert line':
-
-  command => '/usr/bin/sed -i "49i\add_header X-Served-By $HOSTNAME check;" /etc/nginx/sites-available/default',
-
-}
-
-exec {'start_server':
-
-  command => '/usr/bin/sudo /usr/sbin/service nginx restart',
-
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/server_name _;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
