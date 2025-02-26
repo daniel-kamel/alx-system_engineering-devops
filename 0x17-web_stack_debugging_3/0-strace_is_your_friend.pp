f02cf07638a8519e16c18cd62186e0324a7a61f5
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2025-02-26 20:32:14 +0200

    Update script header to reflect project purpose and change file permissions

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-# Fixes the Apache 500 error
+# Solution for 0x17-web_stack_debugging_3 project
+
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
