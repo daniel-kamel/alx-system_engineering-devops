72aa9f2f9a26f4be292125d7b44dd67e469a87d0
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2025-02-28 17:35:56 +0200

    Fix typo in WordPress settings file path and update executable search path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Solution for 0x17-web_stack_debugging_3 project
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i s/.phpp/.php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/:/usr/bin/:/usr/local/sbin/:/sbin/:/usr/sbin/',
 }
